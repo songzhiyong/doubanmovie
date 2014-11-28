d4d46e93f3af77bbdb75eb36fa44eaadd1e11e7f
songzhiyong <songzhiyong1121@gmail.com>
2014-11-29 00:11:57 +0800

    refine json format, insert into es

--- doubanmovie/settings.py
+++ doubanmovie/settings.py
@@ -5,6 +5,7 @@
 #
 #     http://doc.scrapy.org/en/latest/topics/settings.html
 #
+from scrapy import log
 
 BOT_NAME         = 'doubanmovie'
 
@@ -14,10 +15,22 @@ NEWSPIDER_MODULE = 'doubanmovie.spiders'
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'doubanmovie (+http://www.yourdomain.com)'
 
-ITEM_PIPELINES = {
-    'doubanmovie.pipelines.MyImagesPipeline':1,
-    'doubanmovie.pipelines.JsonWithEncodingPipeline': 300
-}
+# ITEM_PIPELINES = {
+#     'doubanmovie.pipelines.MyImagesPipeline':1,
+#     'doubanmovie.pipelines.JsonWithEncodingPipeline': 300
+# }
+
+ITEM_PIPELINES = [
+  'scrapyelasticsearch.scrapyelasticsearch.ElasticSearchPipeline',
+]
+
+ELASTICSEARCH_SERVER = 'localhost' # If not 'localhost' prepend 'http://'
+ELASTICSEARCH_PORT = 9200 # If port 80 leave blank
+ELASTICSEARCH_INDEX = 'doubanmovie'
+ELASTICSEARCH_TYPE = 'movie'
+ELASTICSEARCH_UNIQ_KEY = 'link'
+ELASTICSEARCH_LOG_LEVEL= log.DEBUG
+
 
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.54 Safari/536.5'
 IMAGES_STORE = 'posters-downloaded/'
--- doubanmovie/spiders/movie_spider.py
+++ doubanmovie/spiders/movie_spider.py
@@ -16,14 +16,18 @@ class DoubanMovieSpider(Spider):
         items = []
         for site in sites:
             item         = MovieItem()
-            item['rank'] = site.xpath('div[@class="pic"]/em/text()').extract()
+            item['rank'] = site.xpath('div[@class="pic"]/em/text()').extract()[0]
             title_list   = site.css('.title::text').extract()
             for i in range(0,len(title_list)):
                 title_list[i] = title_list[i].replace('/', '').strip()
-            item['title'] = title_list
-            item['score']  = site.xpath('div[@class="info"]//div[@class="star"]//span/em/text()').extract()
-            item['quote']  = site.xpath('div[@class="info"]//p[@class="quote"]/span/text()').extract()
-            item['link']   = site.xpath('div[@class="info"]/div/a/@href').extract()
+            item['title'] = title_list[0]
+            item['score']  = (float)(site.xpath('div[@class="info"]//div[@class="star"]//span/em/text()').extract()[0])
+            quote = site.xpath('div[@class="info"]//p[@class="quote"]/span/text()').extract()
+            if len(quote)>0:
+                item['quote'] = quote[0]
+            else:
+                item['quote'] =''
+            item['link']   = site.xpath('div[@class="info"]/div/a/@href').extract()[0]
             # http://img3.douban.com/view/photo/thumb/public/p1917567652.jpg
             # http://img3.douban.com/view/photo/photo/public/p1917567652.jpg
             # http://img3.douban.com/view/movie_poster_cover/ipst/public/p1917567652.jpg
@@ -33,6 +37,6 @@ class DoubanMovieSpider(Spider):
                 list[i] = list[i].replace('movie_poster_cover/ipst', 'photo/photo')
                 if 'spic' in list[i]:  
                     list[i] = list[i].replace('spic','mpic')
-            item['cover'] = list
+            item['cover'] = list[0]
             items.append(item)
         return items
